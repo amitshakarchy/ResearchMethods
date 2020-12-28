f7bb975769c84e14bd4fac3b440e738f030963e1
shirshir05 <shirshir05@walla.co.il>
2020-12-28 17:54:59 +0200

    update class and plot and sensitivity analysis

--- PredictingPD.py
+++ PredictingPD.py
@@ -1,100 +1,166 @@
 import numpy as np
-from sklearn import svm
-from sklearn.model_selection import KFold
+from matplotlib import pyplot
+from sklearn import svm, metrics
+from sklearn.metrics import f1_score
+from sklearn.model_selection import KFold, cross_val_predict
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate
+
 from Preprocess import *
 from mlxtend.evaluate import paired_ttest_kfold_cv
 
-
-# region Evaluation methods
-def evaluate_metric(model, model_name):
-    print("*** {} metrics *** ".format(model_name))
-    print("Accuracy: %0.2f " % (np.mean(model['test_accuracy']) * 100))
-    print("Avg precision: {}".format(np.mean(model['test_precision'])))
-    print("Avg recall: {}".format(np.mean(model['test_recall'])))
-    print()
+NUMBER_FOLD = 10
 
 
-def examine_svm_kernels():
-    kernels = ['linear', 'poly', 'rbf', 'sigmoid']
-    for i in range(len(kernels)):
-        svm_model = svm.SVC(kernel=kernels[i], probability=True)
-        scores_kfold_svm = cross_validate(estimator=svm_model, X=x, y=y, cv=kfold, scoring=scoring,
-                                          return_train_score=False)
-        evaluate_metric(scores_kfold_svm, "{} kernel SVM".format(kernels[i]))
+class CompareAlgorithm:
+
+    def __init__(self):
+        self.pre = PreProcessing()
+        self.scoring = ['precision', 'recall', 'f1', 'accuracy']
+        # Perform 10 fold validation
+        self.kfold = KFold(n_splits=NUMBER_FOLD, random_state=100)
+        self.thresholds = [0.3, 0.5, 0.7, 0.9, 0.99]
+
+    def main(self):
+        self.pre_process()
+        self.examine_svm_kernels()
+        self.rf_examine_criterion()
+        self.rf_examine_n_estimators()
+        print("============================================================")
+        print("============================================================")
+        print("SVM vs. Random Forest:")
+        print()
+
+        self.best_svm()
+        self.best_rf()
+        self.statistical_significance_tests()
+        self.sensitivity_analysis(self.svm_model)
+        self.sensitivity_analysis(self.rf_model)
+
+    def pre_process(self):
+        self.pre.pre_process()
+
+    def examine_svm_kernels(self):
+        print("==================== SVM - Examine Parameters ====================")
+        print()
+        print(" 1. ---------- Examine SVM Kernels: ----------")
+        kernels = ['linear', 'poly', 'rbf', 'sigmoid']
+        for i in range(len(kernels)):
+            svm_model = svm.SVC(kernel=kernels[i], probability=True)
+            scores_kfold_svm = cross_validate(estimator=svm_model, X=self.pre.x, y=self.pre.y, cv=self.kfold,
+                                              scoring=self.scoring, return_train_score=False)
+            CompareAlgorithm.evaluate_metric(scores_kfold_svm, "{} kernel SVM".format(kernels[i]))
+        print()
+        print("SVM's best operators:  ")
+        print("Kernel: rbf ")
+
+    def rf_examine_criterion(self):
+        print("==================== RF - Examine Parameters ====================")
+        print()
+        print(" 1. ---------- Examine criterion: ----------")
+        criterion = ['gini', 'entropy']
+        for i in range(len(criterion)):
+            RF_model = RandomForestClassifier(n_estimators=100, criterion=criterion[i])
+            scores_kfold_RF = cross_validate(estimator=RF_model, X=self.pre.x, y=self.pre.y, cv=self.kfold,
+                                             scoring=self.scoring, return_train_score=False)
+            CompareAlgorithm.evaluate_metric(scores_kfold_RF, "{} criterion RF".format(criterion[i]))
+
+    def rf_examine_n_estimators(self):
+        print("2. ---------- Examine n_estimators: ----------")
+        n_estimators = [50, 100, 150]
+        for i in range(len(n_estimators)):
+            RF_model = RandomForestClassifier(n_estimators=n_estimators[i], criterion='entropy', random_state=42)
+            scores_kfold_RF = cross_validate(estimator=RF_model, X=self.pre.x, y=self.pre.y, cv=self.kfold,
+                                             scoring=self.scoring, return_train_score=False)
+            CompareAlgorithm.evaluate_metric(scores_kfold_RF, "{} n_estimators RF".format(n_estimators[i]))
+        print()
+        print("RF's best operators:  ")
+        print("criterion: entropy ")
+        print("n_estimators: 100 ")
+
+    @staticmethod
+    def evaluate_metric(model, model_name):
+        print("*** {} metrics *** ".format(model_name))
+        print("Accuracy: %0.2f " % (np.mean(model['test_accuracy']) * 100))
+        print("Avg precision: {}".format(np.mean(model['test_precision'])))
+        print("Avg recall: {}".format(np.mean(model['test_recall'])))
+        print()
+
+    def best_svm(self):
+        self.svm_model = svm.SVC(kernel='rbf', probability=True)
+        scores_kfold_svm = cross_validate(estimator=self.svm_model, X=self.pre.x, y=self.pre.y, cv=self.kfold,
+                                          scoring=self.scoring, return_train_score=False)
+        CompareAlgorithm.evaluate_metric(scores_kfold_svm, "SVM")
+
+    def best_rf(self):
+        self.rf_model = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=42)
+        scores_kfold_RF = cross_validate(estimator=self.rf_model, X=self.pre.x, y=self.pre.y, cv=self.kfold,
+                                         scoring=self.scoring, return_train_score=False)
+        CompareAlgorithm.evaluate_metric(scores_kfold_RF, "RF")
+
+    def sensitivity_analysis(self, model):
+        print("============================================================")
+        print("============================================================")
+        print("Sensitivity Analysis:")
+        print()
+        predict_prob = cross_val_predict(estimator=model, X=self.pre.x, y=self.pre.y, cv=self.kfold,
+                                         method='predict_proba')
+        list_accuracy, list_precision, list_recall = [], [], []
+        predict_prob = predict_prob[:, 1]
+        for i in self.thresholds:
+            list_prob = []
+            for idx, prob in enumerate(predict_prob):
+                if predict_prob[idx] < i:
+                    list_prob.append(0)
+                else:
+                    list_prob.append(1)
+            acc = metrics.accuracy_score(self.pre.y, list_prob)
+            list_accuracy.append(acc)
+            precision = metrics.precision_score(self.pre.y, list_prob)
+            list_precision.append(precision)
+            recall = metrics.recall_score(self.pre.y, list_prob)
+            list_recall.append(recall)
+            print(f"Thresholds = {i}:")
+            print("Accuracy: %0.2f " % (acc))
+            print("Precision: {}".format(precision))
+            print("Recall: {}".format(recall))
+            print()
+        self.plot_threshold(list_accuracy, list_precision, list_recall)
+
+    def plot_threshold(self, list_accuracy, list_precision, list_recall):
+        # plot loss
+        pyplot.subplot(3, 1, 1)
+        pyplot.scatter(self.thresholds, list_accuracy, color= "green")
+        pyplot.xlabel('Thresholds')
+        pyplot.ylabel('Accuracy')
+        # plot discriminator accuracy
+        pyplot.subplot(3, 1, 2)
+        pyplot.scatter(self.thresholds, list_precision)
+        pyplot.xlabel('Thresholds')
+        pyplot.ylabel('Precision')
+        pyplot.subplot(3, 1, 3)
+        pyplot.scatter(self.thresholds, list_recall, color= "red")
+        pyplot.xlabel('Thresholds')
+        pyplot.ylabel('Recall')
+        # save plot to file
+        pyplot.savefig('plot_threshold.png')
+        pyplot.show()
+        pyplot.close()
+
+    def statistical_significance_tests(self):
+        print("============================================================")
+        print("============================================================")
+        print("K-fold cross-validated paired t-test:")
+        print()
+
+        t, p = paired_ttest_kfold_cv(estimator1=self.svm_model,
+                                     estimator2=self.rf_model,
+                                     X=self.pre.x, y=self.pre.y,
+                                     random_seed=42)
+        print('t statistic: %.3f' % t)
+        print('p value: %.3f' % p)
 
 
-def rf_examine_criterion():
-    criterion = ['gini', 'entropy']
-    for i in range(len(criterion)):
-        RF_model = RandomForestClassifier(n_estimators=100, criterion=criterion[i])
-        scores_kfold_RF = cross_validate(estimator=RF_model, X=x, y=y, cv=kfold, scoring=scoring,
-                                         return_train_score=False)
-        evaluate_metric(scores_kfold_RF, "{} criterion RF".format(criterion[i]))
-
-
-def rf_examine_n_estimators():
-    n_estimators = [50, 100, 150]
-    for i in range(len(n_estimators)):
-        RF_model = RandomForestClassifier(n_estimators=n_estimators[i], criterion='entropy')
-        scores_kfold_RF = cross_validate(estimator=RF_model, X=x, y=y, cv=kfold, scoring=scoring,
-                                         return_train_score=False)
-        evaluate_metric(scores_kfold_RF, "{} n_estimators RF".format(n_estimators[i]))
-# endregion
-
-
-random_seed = 42
-scoring = ['precision', 'recall', 'f1', 'accuracy']
-
-# Perform 10 fold validation
-kfold = KFold(n_splits=10, random_state=100)
-
-# Examine SVM Kernels
-print("==================== SVM - Examine Parameters ====================")
-print()
-print(" 1. ---------- Examine SVM Kernels: ----------")
-examine_svm_kernels()
-print()
-print("SVM's best operators:  ")
-print("Kernel: rbf ")
-
-# Examine RF parameters
-
-print("==================== RF - Examine Parameters ====================")
-print()
-print(" 1. ---------- Examine criterion: ----------")
-rf_examine_criterion()
-print("2. ---------- Examine n_estimators: ----------")
-rf_examine_n_estimators()
-
-print()
-print("RF's best operators:  ")
-print("criterion: entropy ")
-print("n_estimators: 100 ")
-
-print("============================================================")
-print("============================================================")
-print("SVM vs. Random Forest:")
-print()
-svm_model = svm.SVC(kernel='rbf', probability=True)
-scores_kfold_svm = cross_validate(estimator=svm_model, X=x, y=y, cv=kfold, scoring=scoring,
-                                  return_train_score=False)
-evaluate_metric(scores_kfold_svm, "SVM")
-
-RF_model = RandomForestClassifier(n_estimators=100, criterion='entropy')
-scores_kfold_RF = cross_validate(estimator=RF_model, X=x, y=y, cv=kfold, scoring=scoring,
-                                 return_train_score=False)
-evaluate_metric(scores_kfold_RF, "RF")
-
-print("============================================================")
-print("============================================================")
-print("K-fold cross-validated paired t-test:")
-print()
-
-t, p = paired_ttest_kfold_cv(estimator1=svm_model,
-                             estimator2=RF_model,
-                             X=x, y=y,
-                             random_seed=42)
-print('t statistic: %.3f' % t)
-print('p value: %.3f' % p)
+if __name__ == '__main__':
+    compare_algorithm = CompareAlgorithm()
+    compare_algorithm.main()
--- Preprocess.py
+++ Preprocess.py
@@ -2,19 +2,25 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
-# Read the data
-df = pd.read_csv('Resources/parkinsons.data')
 
-# Get the features and labels
-features = df.loc[:, df.columns != 'status'].values[:, 1:]
-labels = df.loc[:, 'status'].values
+class PreProcessing:
 
-# Get the count of each label (0 and 1) in labels
-# print("Parkinson's (one): {}, healthy (zero): {}".format(labels[labels == 1].shape[0], labels[labels == 0].shape[0]))
+    def __init__(self):
+        # Read the data
+        self.df = pd.read_csv('Resources/parkinsons.data')
 
-# Scale the features to between -1 and 1
-scaler = MinMaxScaler((-1, 1))
-x = scaler.fit_transform(features)
-y = labels
-# Split the dataset
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=7)
+    def pre_process(self):
+        # Get the features and labels
+        self.features = self.df.loc[:, self.df.columns != 'status'].values[:, 1:]
+        self.labels = self.df.loc[:, 'status'].values
+
+        # Get the count of each label (0 and 1) in labels
+        # print("Parkinson's (one): {}, healthy (zero): {}".format(labels[labels == 1].shape[0], labels[labels == 0].shape[0]))
+
+        # Scale the features to between -1 and 1
+        self.scaler = MinMaxScaler((-1, 1))
+        self.x = self.scaler.fit_transform(self.features)
+        self.y = self.labels
+        # Split the dataset
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.x, self.y, test_size=0.2,
+                                                                                random_state=7)
